71a505e15aadd3d15fa329c5d7403a300a859837
Rich <rich85020@gmail.com>
2018-07-19 18:15:38 -0400

    Initial commit

---
+++ richent/src/components/forms/LoginForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import Validator from 'validator';
+import InlineError from '../messages/InlineError'
+
+class LoginForm extends React.Component {
+    state = {
+        data: {
+            email:'',
+            password:''
+        },
+        loading: false,
+        errors: {}
+    }
+
+onChange = e => this.setState({ data: { ...this.state.data, [e.target.name]: e.target.value }});
+
+onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+}
+
+validate = (data) => {
+    const errors = {};
+    if (!Validator.isEmail(data.email)) errors.email = 'Invalid Email';
+    if (!data.password) errors.password = "Can't be Blank";
+    return errors
+}
+
+    render() {
+        const {data } = this.state;
+        return(
+            <Form onSubmit={this.onSubmit}>
+                <Form.Field>
+                    <label htmlFor='email'>Email</label>
+                    <input type='email' id='email' name='email' placeholder='example@example.com' value={data.email} onChange={this.onChange}/>
+                </Form.Field>
+                <Form.Field>
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' id='password' name='password' placeholder='Make it Secure' value={data.password} onChange={this.onChange}/>
+                </Form.Field>
+                <Button primary>Login</Button>
+            </Form>
+        );
+    }
+}
+
+export default LoginForm;
---
+++ richent/src/components/messages/InlineError.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const InlineError = ({ text }) => <span>{text}</span>;
+
+export default InlineError;
